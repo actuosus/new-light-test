9d9b5ded7887bbd700d3731a4250b974639a2168
Arthur Chafonov <actuosus@gmail.com>
2025-11-11 13:08:06 +0300

    refactor: update database connection to use pg and drizzle-orm with node-postgres
    - Changed the dependency from 'postgres' to 'pg' in package.json.
    - Updated the database connection implementation in drizzle.ts to use 'pg' Pool.
    - Adjusted drizzle import to use 'drizzle-orm/node-postgres'.

--- src/shared/infrastructure/db/drizzle.ts
+++ src/shared/infrastructure/db/drizzle.ts
@@ -1,4 +1,10 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-export const db = drizzle(postgres(process.env.DATABASE_URL!));
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle({ client: pool });
+
+export default db;
