74ae477f30a0206385458383894d050215490bed
Arthur Chafonov <actuosus@gmail.com>
2025-11-12 07:08:38 +0300

    refactor: simplify pino logger configuration by removing transport options

--- src/shared/infrastructure/logging/pino.ts
+++ src/shared/infrastructure/logging/pino.ts
@@ -1,17 +1,5 @@
 import pino from "pino";
 
-const transport = pino.transport({
-  target: "pino-pretty",
-  options: {
-    colorize: true,
-    translateTime: "SYS:yyyy-mm-dd HH:MM:ss",
-    ignore: "pid,hostname",
-  },
+export const logger = pino({
+  level: process.env.LOG_LEVEL || "info",
 });
-
-export const logger = pino(
-  {
-    level: process.env.LOG_LEVEL || "info",
-  },
-  transport
-);
